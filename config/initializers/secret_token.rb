4ec416cbd4eaee59f3dea8aa16efd00f80092f6b
captproton <carl@captproton.com>
2011-06-04 12:07:52 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mycoupons::Application.config.secret_token = '1e850d16ed620bddabb6147f27fbcc3fe3fef054596dab065068394d5bc93a1907c73a3a04f69219cc317157ab81aa6e64c44d09361427b98a2520f66655839b'
